c3eb8eb40c16d5e77e485bd290f3580bd87d1678
Mariano Robledo <marianorobledo75@gmail.com>
2024-05-23 11:00:30 -0300

    modificacion de jensen

--- src/search.py
+++ src/search.py
@@ -91,8 +91,9 @@ def normalize_vectors(vectors, documents):
 # TODO Calcula la divergencia de Jensen Shannon para dos vectores
 def jensen_shannon_divergence(p, q):
     M : dict[str, float] = {}
+    
     for word in q:
-        if p[word]:
+        if p.get(word):
             M[word]=(p[word]+q[word])/2 #verificar que pasa si la palabra no esta en p
         else:
              M[word]=(q[word])/2
@@ -101,18 +102,13 @@ def jensen_shannon_divergence(p, q):
     KL_Q_M=0
 
     for word in M:
-        if p[word] and q[word]:
+        if p.get(word) and q.get(word):
             KL_P_M += p[word] * math.log2(p[word]/M[word])  #hay que verificar si la palabra esta en p
-            KL_Q_M += q[word] * math.log2(q[word]/M[word])  #hay que verificar si la palabra esta en q
-        elif p[word]:
-            KL_P_M += p[word] * math.log2(p[word]/M[word])  #hay que verificar si la palabra esta en p
-            KL_Q_M += 0
-        elif q[word]:
-            KL_P_M += 0                                       #hay que verificar si la palabra esta en p
+            KL_Q_M += q[word] * math.log2(q[word]/M[word])  
+        elif q.get(word):
+            KL_P_M += 0                                       #hay que verificar si la palabra esta en q
             KL_Q_M += q[word] * math.log2(q[word]/M[word])
-        else:
-            KL_P_M += 0
-            KL_Q_M += 0
+        
 
     
     divergence=(KL_P_M + KL_Q_M)/2
